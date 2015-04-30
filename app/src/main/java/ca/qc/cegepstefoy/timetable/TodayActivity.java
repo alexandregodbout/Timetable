b198fdb626196eb7a1e4c0bbc2f65f0604f162d8
Alexandre Godbout <alexandre.godbout.tim@gmail.com>
2015-04-29 23:48:28 -0400

    Added a rough implementation of the toolbar

--- app/src/main/java/ca/qc/cegepstefoy/timetable/TodayActivity.java
+++ app/src/main/java/ca/qc/cegepstefoy/timetable/TodayActivity.java
@@ -7,12 +7,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class CourseActivity extends ActionBarActivity {
+public class TodayActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_course);
+        setContentView(R.layout.activity_today);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         if (toolbar != null) {
@@ -24,7 +24,7 @@ public class CourseActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_course, menu);
+        getMenuInflater().inflate(R.menu.menu_today, menu);
         return true;
     }
 
